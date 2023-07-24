1e1a686bacdc339ff7b1b8f9c795b5d610998a00
= <=>
2023-07-25 00:52:03 +0100

    add utility subcommands to config command

--- src/config/cli.rs
+++ src/config/cli.rs
@@ -1,6 +1,11 @@
 use clap::{Args, Subcommand};
 
-use crate::{cli::{Runnable, Cli}, state::State, modules::sleep::cli::SleepArgs, fs::{CONFIG_PATH, file_contents}};
+use crate::{
+    cli::{Cli, Runnable},
+    fs::{file_contents, CONFIG_PATH},
+    modules::sleep::cli::SleepArgs,
+    state::State,
+};
 
 use super::{Config, Configurable};
 
@@ -8,17 +13,18 @@ use super::{Config, Configurable};
 pub struct ConfigCli {
     #[clap(subcommand)]
     subcmd: Option<SubCommand>,
-
-    #[clap(long, short)]
-    editor: bool,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum SubCommand {
     #[clap(visible_aliases = &["s"])]
+    /// configure sleep settings
     Sleep(SleepArgs),
+    /// open the config file in your editor
+    Edit,
+    /// print the path to the config file
+    Path
 }
-
 
 impl Runnable for ConfigCli {
     type Args = Cli;
@@ -26,25 +32,28 @@ impl Runnable for ConfigCli {
         match &self.subcmd {
             Some(subcmd) => match subcmd {
                 SubCommand::Sleep(sleep_args) => sleep_args.run(&(), state)?,
+                SubCommand::Edit => {
+                    let new_config = inquire::Editor::new(&format!(
+                        "Starting editor at {}",
+                        CONFIG_PATH.display()
+                    ))
+                    .with_predefined_text(&serde_json::to_string_pretty(&state.config)?)
+                    .with_validator(|contents: &str| {
+                        match serde_json::from_str::<crate::config::Config>(&contents) {
+                            Ok(_) => Ok(inquire::validator::Validation::Valid),
+                            Err(e) => Ok(inquire::validator::Validation::Invalid(e.into())),
+                        }
+                    })
+                    .prompt()?;
+
+                    state.config = serde_json::from_str(&new_config)?;
+                },
+                SubCommand::Path => {
+                    println!("Configuration File: {}", CONFIG_PATH.display());
+                }
             },
             None => {
-                if self.editor {
-                    let new_config = inquire::Editor::new(
-                        &format!("Starting editor at {}", CONFIG_PATH.display()),
-                    ).with_predefined_text(&serde_json::to_string_pretty(&state.config)?)
-                    .with_validator(
-                        |contents: &str| {
-                            match serde_json::from_str::<crate::config::Config>(&contents) {
-                                Ok(_) => Ok(inquire::validator::Validation::Valid),
-                                Err(e) => Ok(inquire::validator::Validation::Invalid(e.into())),
-                            }
-                        }
-                    ).prompt()?;
-    
-                    state.config = serde_json::from_str(&new_config)?;
-                } else {
-                    state.config.run_optional_configurator()?;
-                }
+                state.config.run_optional_configurator()?;
             }
         };
 
@@ -59,14 +68,23 @@ impl Config {
             Some(contents) => {
                 let config: Config = serde_json::from_str(&contents)?;
                 Ok(config)
-            },
+            }
             None => {
-                if inquire::Confirm::new("No config file found. Create one?").with_default(true).prompt()? {
+                if inquire::Confirm::new("No config file found. Create one?")
+                    .with_default(true)
+                    .prompt()?
+                {
                     let mut config = Config::default();
                     config.run_configurator()?;
                     config.save()?;
-                    println!("Config file created at {}, running configurator", CONFIG_PATH.display());
-                    match inquire::Confirm::new("Would you like to continue execution?").with_default(true).prompt()? {
+                    println!(
+                        "Config file created at {}, running configurator",
+                        CONFIG_PATH.display()
+                    );
+                    match inquire::Confirm::new("Would you like to continue execution?")
+                        .with_default(true)
+                        .prompt()?
+                    {
                         true => Ok(config),
                         false => Err(anyhow::anyhow!("Execution cancelled")),
                     }
@@ -83,4 +101,3 @@ impl Config {
         Ok(())
     }
 }
-
